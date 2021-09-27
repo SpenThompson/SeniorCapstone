3052517a20a2824665209f2400192e1f11f34c02
Spencer <james.spencer.thompson@gmail.com>
2021-09-26 19:38:25 -0500

    Creating Landing Page Template

--- dnd_site/home/views.py
+++ dnd_site/home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django import forms
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
 
-def index(request):
-    return HttpResponse("Hello, world!  This is the home page!")
+def landing(request):
+    return render(request, "home/landing.html")
 # Create your views here.
