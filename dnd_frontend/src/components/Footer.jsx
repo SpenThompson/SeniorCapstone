8175312dce8560098032bb264e8cc96230248113
Spencer <thompsonjj@hendrix.edu>
2021-11-15 14:57:12 -0600

    Working on Footer

--- dnd_frontend/src/components/Footer.jsx
+++ dnd_frontend/src/components/Footer.jsx
@@ -1,49 +1,49 @@
 import React from "react";
-import { siteFooter, footerLinks, footerLink } from './FooterElements';
+import { 
+   footerBox,
+   footerHeading,
+   footerText,
+   footerContainer,
+   footerColumn,
+   footerRow,
+   footerLink } from './FooterElements';
 
 function Footer(props) {
     return (
-        <div className="footer">
-            <siteFooter>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <h6>About</h6>
-                        <p class="text-justify">PLACEHOLDER</p>
-                    </div>
-                    <div class="col-xs-6 col-md-3">
-                        <h6>Headings</h6>
-                        <footerLinks>
-                            <li><footerLink to="/">Home</footerLink></li>
-                            <li><footerLink to="/members">Members</footerLink></li>
-                            <li><footerLink to="/information">Information</footerLink></li>
-                            <li><footerLink to="/services">Services</footerLink></li>
-                            <li><footerLink to="/contact">Contact Us</footerLink></li>
-                        </footerLinks>
-                    </div>
-                    <div class="col-xs-6 col-md-3">
-                        <h6>Member Links</h6>
-                        <footerLinks>
-                            <li><footerLink to="/members/looking-for-game">Looking for Game</footerLink></li>
-                            <li><footerLink to="/members/looking-for-players">Looking for Players</footerLink></li>
-                            <li><footerLink to="/members/submit-homebrew">Submit Homebrew</footerLink></li>
-                            <li><footerLink to="/members/open-games">Open Games</footerLink></li>
-                            <li><footerLink to="/members/available-players">Available Players</footerLink></li>
-                            <li><footerLink to="/members/homebrew-archive">Homebrew Archive</footerLink></li>
-                        </footerLinks>
-                    </div>
-                    <hr></hr>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-sm-6 col-xs-12">
-                                <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by Spencer Thompson.</p>
-                            </div>
-                        </div>
-                    </div>
+        <footerBox>
+          <footerContainer>
+            <footerRow>
+              <div class="container">
+                <div class="col-md-6">
+                  <footerHeading>About</footerHeading>
+                  <footerText>PLACEHOLDER</footerText>
                 </div>
+              </div>
+              <footerColumn>
+                <footerHeading>Quick Forms</footerHeading>
+                <footerLink to="/members/interest">Interest Form</footerLink>
+                <footerLink to="/members/looking-for-game">Looking for Game</footerLink>
+                <footerLink to="/members/looking-for-players">Looking for Players</footerLink>
+                <footerLink to="/members/submit-homebrew">Submit Homebrew</footerLink>
+              </footerColumn>
+              <footerColumn>
+                <footerHeading>Member Tools</footerHeading>
+                <footerLink to="/members/open-games">Open Games</footerLink>
+                <footerLink to="/members/interested-players">Interested Players</footerLink>
+                <footerLink to="/members/homebrew-archive">Homebrew Archive</footerLink>
+              </footerColumn>
+            </footerRow>
+          </footerContainer>
+          <footerRow>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                  <footerText>Copyright &copy; 2021 All Rights Reserved by Spencer Thompson.</footerText>
+                </div>
+              </div>
             </div>
-            </siteFooter>
-        </div>
+          </footerRow>
+        </footerBox>
     )
  }
 
--- dnd_frontend/src/components/FooterElements.jsx
+++ dnd_frontend/src/components/FooterElements.jsx
@@ -1,40 +1,59 @@
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const siteFooter = styled.section`
-  background-color:#26272b;
-  padding:45px 0 20px;
-  font-size:15px;
-  line-height:24px;
-  color:#737373; 
+export const footerBox = styled.div`
+  padding: 80px 60px;
+  background: #26272b;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+export const footerRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
+  grid-gap: 20px; 
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+export const footerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+export const footerColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
 `;
 
 export const footerLink = styled(Link)`
-  color: #737373;
-  display: flex;
-  align-items: center;
+  color: #fff;
+  margin-bottom: 20px;
   text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  cursor: pointer;
-  &.inline li{
-    display:inline-block;
+  &:hover {
+    color: #E96B10;
+    transition: 200ms ease-in;
   }
   &.active {
-    color: #444444;
+    color: #E96B10;
   }
 `;
 
-export const footerLinks = styled.ul`
-    padding-left:0;
-    list-style:none;
-    .li{
-        display:block;
-    }
-    .active {
-        color: #444444;
-    }
+export const footerHeading = styled.h6`
+    color: #fff;
+    margin-bottom: 40px;
+    font-weight: bold;
+`;
 
+export const footerText = styled.p`
+    color: #fff;
 `;
 
 
