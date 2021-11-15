23c020cad2f12d0178684f5df67dd0222c212e20
Spencer <thompsonjj@hendrix.edu>
2021-11-15 15:31:30 -0600

    Updated File Structure

---
+++ dnd_frontend/src/components/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Information } from "./Information";
+export { default as Members } from "./Members";
+export { default as Services } from "./Services";
+export { default as Contact } from "./Contact";
